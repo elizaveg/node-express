419e55a29242404ed5d2402d2ce33c280e59ddfc
elizaveg <100614294+elizaveg@users.noreply.github.com>
2022-12-09 10:52:49 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import './index.js';
+
+//Import Components
+import First from "./components/First";
+
+function App() {
+  return (
+    <div className="App">
+      <First />
+    </div>
+  );
+}
+
+export default App;
